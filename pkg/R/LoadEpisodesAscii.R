4af9efc753b9695fe8ac4ad40c68d1e7f6919175
leandroide <leandroide@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-21 19:39:26 +0000

    Solved a problem when episodes don't have a numerical value associated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhrv@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/LoadEpisodesAscii.R
+++ pkg/R/LoadEpisodesAscii.R
@@ -27,16 +27,17 @@ function(HRVData, FileName, Tag="", InitTime="0:0:0", verbose=NULL) {
 	}
 	x=read.table(FileName,skip=1)
 
-   	if (HRVData$Verbose) {
-      	if (Tag=="") {
-         	cat("   No tag specified\n")
-      	} else {
-         	cat("   Tag:",Tag,"\n")
-      	}
+	if (HRVData$Verbose) {
+   	if (Tag=="") {
+      	cat("   No tag specified\n")
+   	} else {
+      	cat("   Tag:",Tag,"\n")
    	}
+	}
 
 	# obtaining time
-	timeaux = strptime(InitTime,"%H:%M:%S")
+   op <- options(digits.secs=3)
+	timeaux = strptime(InitTime,"%H:%M:%OS")
 	if (is.na(timeaux)) {
 		cat("   --- ERROR: Time format is HH:MM:SS ---\n")
 		return(HRVData)
@@ -45,39 +46,54 @@ function(HRVData, FileName, Tag="", InitTime="0:0:0", verbose=NULL) {
 	if (HRVData$Verbose) {
 		cat("   Initial time: ",sprintf("%02d",timeaux$hour),":",
 			sprintf("%02d",timeaux$min),":",
-			sprintf("%02d",timeaux$sec),"\n",sep="")
+			sprintf("%02.3f",timeaux$sec),"\n",sep="")
 	}
 
 	# calculating time in seconds, considering the initial time for the register
-	EpisodeTimeAbs=strptime(x$V1,"%T")
-	EpisodeTimeRel=difftime(EpisodeTimeAbs,timeaux, units="secs")
-   	x$V1=as.numeric(EpisodeTimeRel)
 
-   	if (Tag=="") {
-      	y=x
-   	} else {
-      	y=subset(x,x$V2==Tag)
-   	}
+	EpisodeTimeAbs=strptime(x$V1,"%H:%M:%OS")
+	EpisodeTimeRel=difftime(EpisodeTimeAbs,timeaux, units="secs")
+	x$V1=as.numeric(EpisodeTimeRel)
+
+	if (Tag=="") {
+   	y=x
+	} else {
+   	y=subset(x,x$V2==Tag)
+	}
    	
 	added=length(y$V1)
 
-   	if (added==0) {
-      	if (HRVData$Verbose) {
-         	cat("   No episode was loaded\n")
-      	}
-   	} else {
-      	HRVData$Episodes=rbind(HRVData$Episodes,data.frame(InitTime=y$V1,Type=y$V2,Duration=y$V3,Value=y$V4))
-      	HRVData$Episodes=HRVData$Episodes[order(HRVData$Episodes$InitTime),]  # Sorts episodes by InitTime
-      	HRVData$Episodes=HRVData$Episodes[!duplicated(HRVData$Episodes),]  # Removes duplicated episodes
-      
-      	if (HRVData$Verbose) {
-         	cat("   Loaded",added,"episodes from file\n")
-      	}
-   	}
+   if (HRVData$Verbose) {
+      if (is.null(y$V4)) {
+         cat("   Data does not include values associated to episodes\n")
+      } else {
+         cat("   Data includes values associated to episodes\n")
+      }
+   }
 
+	if (added==0) {
    	if (HRVData$Verbose) {
-      	cat("   Number of episodes:",length(HRVData$Episodes$InitTime),"\n")
+      	cat("   No episode was loaded\n")
    	}
+	} else {
+
+      if (is.null(y$V4)) {
+         HRVData$Episodes=rbind(HRVData$Episodes,data.frame(InitTime=y$V1,Type=y$V2,Duration=y$V3))
+      } else {
+   	  HRVData$Episodes=rbind(HRVData$Episodes,data.frame(InitTime=y$V1,Type=y$V2,Duration=y$V3,Value=y$V4))
+      }
+
+   	HRVData$Episodes=HRVData$Episodes[order(HRVData$Episodes$InitTime),]  # Sorts episodes by InitTime
+   	HRVData$Episodes=HRVData$Episodes[!duplicated(HRVData$Episodes),]  # Removes duplicated episodes
+   
+   	if (HRVData$Verbose) {
+      	cat("   Loaded",added,"episodes from file\n")
+   	}
+	}
+
+	if (HRVData$Verbose) {
+   	cat("   Number of episodes:",length(HRVData$Episodes$InitTime),"\n")
+	}
 
 	return(HRVData)
 }
