06739c30295af8d80263f6b274e1b533dbdfda9a
leandroide <leandroide@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-08 14:05:44 +0000

    Solved problem with frames beloging to an episode
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhrv@174 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/SplitPowerBandByEpisodes.R
+++ pkg/R/SplitPowerBandByEpisodes.R
@@ -17,11 +17,11 @@ function(HRVData, indexFreqAnalysis = length(HRVData$FreqAnalysis), Tag="", verb
 	}
 
    if (is.null(HRVData$Episodes)) {
-      stop("  --- Episodes not present\n    --- Quitting now!! ---\n")
+      stop("  --- Episodes not present ---\n    --- Quitting now!! ---\n")
    }
 
 	if (is.null(HRVData$FreqAnalysis[[indexFreqAnalysis]]$ULF)) {
-      stop("  --- Power per band not present\n    --- Quitting now!! ---\n")
+      stop("  --- Power per band not present ---\n    --- Quitting now!! ---\n")
 	}
 
 	if (HRVData$Verbose) {
@@ -43,20 +43,50 @@ function(HRVData, indexFreqAnalysis = length(HRVData$FreqAnalysis), Tag="", verb
       cat("   Number of episodes:",length(ActiveEpisodes$InitTime),"\n")
    }
 
+
+
    lframes=length(HRVData$FreqAnalysis[[indexFreqAnalysis]]$HRV)
    # lframes is the number of frames
 
-   EpisodesLeft=ActiveEpisodes$InitTime # Beg of episodes (seconds)
-   EpisodesLeftFrame=EpisodesLeft*lframes/(tail(HRVData$Beat$Time,1)-head(HRVData$Beat$Time,1)) # Beg of episodes (frames)
-   EpisodesRight=ActiveEpisodes$InitTime+ActiveEpisodes$Duration # Beg of episodes (seconds)
-   EpisodesRightFrame=EpisodesRight*lframes/(tail(HRVData$Beat$Time,1)-head(HRVData$Beat$Time,1)) # Beg of episodes (frames)
+   # cat("No. of frames: ", lframes, "\n")
+   # cat("Beginning of file: ",head(HRVData$Beat$Time,1),"\n")
+   # cat("End of file: ",tail(HRVData$Beat$Time,1),"\n")
 
    index=c()
-   for (i in 1:length(EpisodesLeft)) {
-      index=c(index,EpisodesLeftFrame[i]:EpisodesRightFrame[i])
+
+   for (i in 1:lframes) {
+      BegOfFrame = head(HRVData$Beat$Time,1)+HRVData$FreqAnalysis[[indexFreqAnalysis]]$shift*(i-1)
+      EndOfFrame = BegOfFrame + HRVData$FreqAnalysis[[indexFreqAnalysis]]$size
+      # if (i<10 || i>lframes-10) {
+      #    cat("Frame: ",i,"  -  ")
+      #    cat("CenterOfFrame: ",CenterOfFrame,"\n")
+      # }
+      inEp = FALSE
+      if (length(ActiveEpisodes$InitTime)>0) {
+         for (j in 1:length(ActiveEpisodes$InitTime)) {
+            begEp = ActiveEpisodes$InitTime[j]
+            endEp = ActiveEpisodes$InitTime[j]+ActiveEpisodes$Duration[j]
+            if (BegOfFrame>=begEp && EndOfFrame<=endEp) {
+               inEp = TRUE
+            }
+         }
+      }
+
+      if (inEp) {
+         # cat("Frame in episode: ",i,"  -  ")
+         # cat("Beg of frame: ",BegOfFrame,"  -  ")
+         # cat("End of frame: ",EndOfFrame,"\n")
+         index=c(index,i)
+      }
+
+   }
+
+   if (length(index)==0){
+      cat("   --- Warning: no frames in episodes with tag '",Tag,"'!! ---\n",sep="")
    }
 
    l=list()
+
 
    l$InEpisodes=list(ULF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$ULF[index],
       VLF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$VLF[index],
@@ -64,11 +94,20 @@ function(HRVData, indexFreqAnalysis = length(HRVData$FreqAnalysis), Tag="", verb
       HF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$HF[index]
    )
 
-   l$OutEpisodes=list(ULF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$ULF[-index],
-      VLF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$VLF[-index],
-      LF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$LF[-index],
-      HF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$HF[-index]
-   )
+   if (length(index)==0){
+      l$OutEpisodes=list(ULF=c(HRVData$FreqAnalysis[[indexFreqAnalysis]]$ULF),
+         c(VLF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$VLF),
+         c(LF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$LF),
+         c(HF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$HF)
+      )
+   } else {
+      l$OutEpisodes=list(ULF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$ULF[-index],
+         VLF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$VLF[-index],
+         LF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$LF[-index],
+         HF=HRVData$FreqAnalysis[[indexFreqAnalysis]]$HF[-index]
+      )   
+   }
+
 
    if (HRVData$Verbose) {
       cat("   No. of frames:",lframes,"\n")
