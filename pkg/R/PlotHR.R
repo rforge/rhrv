c82bb4229aaa0bea4761ee82c4302b559114f2e5
leandroide <leandroide@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-06 10:30:49 +0000

    PlotHR allows to select episodes using tags or indexes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhrv@190 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/PlotHR.R
+++ pkg/R/PlotHR.R
@@ -4,11 +4,12 @@
 #'  rate signal.
 #' @param HRVData Data structure that stores the beats register and information 
 #' related to it.
-#' @param Tag List of tags to specify which episodes, as apnoea or oxygen 
-#' desaturation, are included in the plot. Tag="all" plots all episodes 
-#' present in the data. 
-#' @param verbose Deprecated argument maintained for compatibility, use 
-#' SetVerbose() instead.
+#' @param Tags List of tags to specify which episodes, as apnoea or oxygen 
+#' desaturation, are included in the plot. Tags="all" plots all episodes present
+#' in the data. 
+#' @param Indexes List of indexes to specify which episodes (see ListEpisodes),
+#' are included in the plot. Indexes="all" plots all episodes present
+#' in the data. 
 #' @param main A main title for the plot.
 #' @param xlab A label for the x axis.
 #' @param ylab a label for the y axis
@@ -24,27 +25,29 @@
 #' @author M. Lado, A. Mendez, D. Olivieri, L. Rodriguez, X. Vila, C.A. Garcia
 #' @keywords aplot
 PlotHR <-
-function(HRVData, Tag=NULL, verbose=NULL, 
+function(HRVData, Tags=NULL, Indexes=NULL,
          main = "Interpolated instantaneous heart rate",
          xlab="time (sec.)", ylab="HR (beats/min.)",type="l",ylim=NULL, ...) {
 # -----------------------------
 # Plots interpolated Heart Rate
 # -----------------------------
-#	Tag -> Tags of episodes to include in the plot
+#	Tags -> Tags of episodes to include in the plot
 #    "all" includes all types
 
 
-	if (!is.null(verbose)) {
-		cat("  --- Warning: deprecated argument, using SetVerbose() instead ---\n
-        --- See help for more information!! ---\n")
-		SetVerbose(HRVData,verbose)
+	if (hasArg(verbose)) {
+		stop("Deprecated argument 'Verbose': use SetVerbose() instead (see help)")
 	}
-	
+
+	if (hasArg(Tag)) {
+		Tags <- Tag
+	}
+
 	if (HRVData$Verbose) {
 		cat("** Plotting interpolated instantaneous heart rate **\n");
 	}
    
-   if (!is.null(Tag) & is.null(HRVData$Episodes)) {
+   if ((!is.null(Tags) || !is.null(Indexes)) & is.null(HRVData$Episodes)) {
       stop("  --- Episodes not present ---\n    --- Quitting now!! ---\n")
    }
 
@@ -66,15 +69,6 @@ function(HRVData, Tag=NULL, verbose=NULL,
 	HRMax=max(HRVData$HR)
 	HRDiff=HRMax-HRMin
 
-	if (!is.null(Tag)) {
-		if (Tag[1]=="all") {
-			Tag=levels(HRVData$Episodes$Type)
-		}
-
-		if (HRVData$Verbose) {
-			cat("   Episodes in plot:",Tag,"\n")
-		}
-	}
 
   if (is.null(ylim)){
     ylim = c(HRMin-0.1*HRDiff,HRMax)
@@ -83,29 +77,40 @@ function(HRVData, Tag=NULL, verbose=NULL,
        ylim=ylim, ...)
 	grid()
 	
-	if (!is.null(Tag)) {
+	if (!is.null(Tags) || !is.null(Indexes)) {
 		# Data for representing episodes
-		EpisodesAuxLeft=HRVData$Episodes$InitTime[HRVData$Episodes$Type %in% Tag]
-		EpisodesAuxBottom=c(HRMin-0.09*HRDiff,HRMin-0.04*HRDiff)
-		EpisodesAuxRight=HRVData$Episodes$InitTime[HRVData$Episodes$Type %in% Tag] + 
-			HRVData$Episodes$Duration[HRVData$Episodes$Type %in% Tag]
-		EpisodesAuxTop=c(HRMin-0.07*HRDiff,HRMin-0.02*HRDiff)
-		EpisodesAuxType=HRVData$Episodes$Type[HRVData$Episodes$Type %in% Tag]
+		EpisodesToPlot <- selectEpisodes(HRVData$Episodes,Tags,Indexes)
+		EpisodesToPlot <- EpisodesToPlot[EpisodesToPlot$selected,]
 
-		Pal=rainbow(length(Tag))
-		Bor=Pal[match(EpisodesAuxType,Tag)]
+		# Data for representing episodes
+		EpisodesAuxLeft <- EpisodesToPlot$InitTime
+		EpisodesAuxBottom <- c(HRMin-0.09*HRDiff,HRMin-0.04*HRDiff)
+		EpisodesAuxRight <- EpisodesToPlot$InitTime + EpisodesToPlot$Duration
+		EpisodesAuxTop <- c(HRMin-0.07*HRDiff,HRMin-0.02*HRDiff)
+		EpisodesAuxType <- EpisodesToPlot$Type
 
-		cat("   No of episodes:",length(EpisodesAuxLeft),"\n")
-		cat("   No of classes of episodes:",length(Pal),"\n")
+		labels <- levels(factor(EpisodesAuxType))
 
-		rect(EpisodesAuxLeft,EpisodesAuxBottom,EpisodesAuxRight,EpisodesAuxTop,border=Bor,col=Bor)
+	 	Pal=rainbow(length(labels))
+	 	Bor=Pal[match(EpisodesAuxType,labels)]
+
+	 	if (HRVData$Verbose) {
+			cat("   No of episodes:",length(EpisodesAuxLeft),"\n")
+			cat("   No of classes of episodes:",length(Pal),"\n")
+		}
+
+		if (length(EpisodesAuxLeft)==1) {
+			rect(EpisodesAuxLeft,EpisodesAuxBottom[1],EpisodesAuxRight,EpisodesAuxTop[1],border=Bor,col=Bor)
+		} else {
+			rect(EpisodesAuxLeft,EpisodesAuxBottom,EpisodesAuxRight,EpisodesAuxTop,border=Bor,col=Bor)
+		}
+
 		for (i in 1:length(EpisodesAuxLeft)) {
-
 			lines(rep(EpisodesAuxLeft[i],times=2),c(HRMin-0.1*HRDiff,HRMax),lty=2,col=Bor[i])
 			lines(rep(EpisodesAuxRight[i],times=2),c(HRMin-0.1*HRDiff,HRMax),lty=2,col=Bor[i])
 		}
 
-		legend("topright",inset=0.01,legend=Tag,fill=Pal,cex=0.6,horiz=FALSE,bg='white')
+		legend("topright",inset=0.01,legend=labels,fill=Pal,cex=0.6,horiz=FALSE,bg='white')
 	}
 
 	title(main=main)
